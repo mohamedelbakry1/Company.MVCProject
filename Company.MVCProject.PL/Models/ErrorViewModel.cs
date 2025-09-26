3322f53500171d306bd08019975ac83f14678e80
Mohamed Elbakary <albqrym58@gmail.com>
2025-09-26 20:10:55 +0300

    Add project files.

---
+++ Company.MVCProject.PL/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Company.MVCProject.PL.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
